a3e56db6657104d01b7319a012a6f030b1e1d402
Artem <artem.suchov@gmail.com>
2012-08-14 22:02:03 +0300

    what

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rm::Application.routes.draw do
+  root :to => 'static_pages#home'
+
+  get "static_pages/home"
+
+  get "static_pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
